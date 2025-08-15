ad26bb0ed519d41369826e33ea0154e0a32e14cd
Shivansh <shivansh.agw@gmail.com>
2025-08-16 03:00:20 +0530

    updated links

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://secure-messenger-backend.onrender.com";
+  process.env.REACT_APP_API_BASE_URL
 
 export const SOCKET_URL =
   process.env.REACT_APP_SOCKET_URL || API_BASE_URL;
